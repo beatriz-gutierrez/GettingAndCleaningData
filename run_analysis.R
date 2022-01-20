3ade65b952bb9f274ebf5fc3e915d57506ed2034
BeaGuti <beatriz.gutierrez@gmail.com>
2022-01-20 21:06:17 +0100

    Use working directory to unzip the file.

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,7 @@ file_url = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%2
 zip_file <- ".\\getdata_projectfiles_UCI HAR Dataset.zip"
 if (!file.exists(zip_file)) { 
         download.file(file_url, destfile = zip_file, mode= 'wb')
-        unzip(zip_file)
+        unzip(zip_file, exdir = getwd())
         date_download <- date()
 }
 data_path <- ".\\UCI HAR Dataset"
